fcd82a3020abd9f46c64ca936d40ef2ad41e135c
wquynguy <wquyenle@gmail.com>
2024-03-28 09:02:35 -0500

    implemented bootstrap navbar and carousel

--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
 console.log("main.js loaded");
 
 $(document).ready(function() {
-    var carousel = $('.carousel');
-    var carouselItems = $('.carousel-item');
-    var currentIndex = 0;
-    var totalItems = carouselItems.length;
-    var itemWidth = carouselItems.width();
-
-    function slideCarousel() {
-        var newPosition = -(currentIndex * itemWidth);
-        carousel.css('transform', 'translateX(' + newPosition + 'px)');
+    for (var i = 0; i < 4; i++) {
+      var card = $('<div class="card">Card ' + (i + 1) + '</div>');
+      $('#card-container').append(card);
     }
-
-    $('.next').click(function() {
-        if (currentIndex < totalItems - 1) {
-            currentIndex++;
-            slideCarousel();
-        }
-    });
-
-    $('.prev').click(function() {
-        if (currentIndex > 0) {
-            currentIndex--;
-            slideCarousel();
-        }
-    });
-});
+  });
